f423af1b24933eb502b46ad352eff270a482e5e0
neelesh <neelesh.dooraree@googlemail.com>
2021-04-17 00:36:23 +0100

    Pause Menu Working

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,18 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
+
+	public bool canPause = true;
+	public bool gameIsPaused = false;
+	public GameObject pauseMenuGO;
+
+	private float timeScaleBeforePause = 1;
+
 	public void Reload() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
 	public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
-
-	public void LoadGameplay()
-	{
-		SceneManager.LoadScene("Gameplay");
-	}
-
+	public void LoadGameplay() => SceneManager.LoadScene("Gameplay");
 	public void SetSinglePlayer() => GameData.Instance.SetSinglePlayer();
 	public void SetTwoPlayer() => GameData.Instance.SetTwoPlayer();
 	public void SetAIvsAI() => GameData.Instance.SetAIvsAI();
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (pauseMenuGO == null) return;
+			if (canPause == false) return;
+
+			if (!gameIsPaused)
+			{
+				timeScaleBeforePause = Time.timeScale;
+				pauseMenuGO.SetActive(true);
+				Time.timeScale = 0f;
+				gameIsPaused = true;
+			}
+			else
+			{
+				pauseMenuGO.SetActive(false);
+				Time.timeScale = timeScaleBeforePause;
+				gameIsPaused = false;
+			}
+		}
+	}
+
+	public void ResumeGame()
+	{
+		gameIsPaused = false;
+	}
+
+	public void SetCanPause(bool canPause)
+	{
+		this.canPause = canPause;
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
 }
